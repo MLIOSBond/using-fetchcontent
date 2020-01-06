e8b9b67bb3d98922750816137fee798a5bb29667
MLIOSBond <bond31337@gmail.com>
2020-01-06 15:59:59 +0300

    add FetchContent

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "submodule-test/somelib.h"
 
 int main()
 {
     std::cout << "Hi there!" << "\n";
+    std::cout << "Squared of 5: " << squared(5) << "\n";
     return 0;
 }
